759310c3746e3af218d661520d6d04e000a56fa7
akmyrzza <akmyrzza@gmail.com>
2023-11-03 09:28:50 +0600

    m

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -10,7 +10,7 @@ func main() {
 	router := http.NewServeMux()
 	router.HandleFunc("/", Handle)
 
-	err := http.ListenAndServe("localhost:8080", router)
+	err := http.ListenAndServe("localhost:8080", router)//ffff
 	if err != nil {
 		panic(err)
 	}

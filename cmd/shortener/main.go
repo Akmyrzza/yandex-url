8a316ced5344a470f68b380d02b89c81f2e991c1
Akmyrzza <123244431+Akmyrzza@users.noreply.github.com>
2023-11-03 09:27:18 +0600

    Update main.go

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,28 +1,5 @@
 package main
 
-import (
-	"net/http"
-
-	"github.com/akmyrzza/yandex-url/internal/handler"
-)
-
 func main() {
-	router := http.NewServeMux()
-	router.HandleFunc("/", Handle)
 
-	err := http.ListenAndServe("localhost:8080", router)
-	if err != nil {
-		panic(err)
-	}
-}
-
-func Handle(w http.ResponseWriter, r *http.Request) {
-	switch r.Method {
-	case "POST":
-		handler.SaveURL(w, r)
-	case "GET":
-		handler.ReturnURL(w, r)
-	default:
-		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
-	}
 }

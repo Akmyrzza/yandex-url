b68c25cc72a3c0ef95bfe96811fcafe74c7be573
akmyrzza <akmyrzza@gmail.com>
2023-11-18 19:11:30 +0600

    logger zap, fix middleware package name

--- internal/router/router.go
+++ internal/router/router.go
@@ -1,16 +1,16 @@
 package router
 
 import (
+	middleware "github.com/akmyrzza/yandex-url/internal/Middleware"
 	"github.com/akmyrzza/yandex-url/internal/handler"
 	"github.com/akmyrzza/yandex-url/internal/logger"
-	"github.com/akmyrzza/yandex-url/internal/middleware"
 	"github.com/gin-gonic/gin"
 )
 
 func InitRouter(h *handler.Handler, lg *logger.Logger) *gin.Engine {
 	router := gin.Default()
 
-	router.Use(middleware.LoggingRequest(lg))
+	router.Use(middleware.LoggingRequest(lg), gin.Recovery())
 
 	router.POST("/", h.ShortURL)
 	router.GET("/:id", h.OriginalURL)
